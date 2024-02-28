9802a068a41958aa8bf9128876ce759d71912643
fisherbyu <fisherandrew777@gmail.com>
2024-02-28 13:14:30 -0700

    add music pages

---
+++ app/music/artists/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import MusicDisplay from "@/components/ui/music-display";
+import content from "@/app/music/artists/content.json";
+
+
+export default function ArtistPage() {
+	return (
+		<main>
+			{ content.map((artist, index) => (
+				<MusicDisplay key={ artist.title } index={ index } components={ artist } />
+			)) }
+		</main>
+	);
+}
+
+export let metadata: Metadata = {
+    title: "My Favorite Artists"
+};
---
+++ app/music/coldplay/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import MusicDisplay from "@/components/ui/music-display";
+
+// Import Content
+import content from "@/app/music/coldplay/content.json";
+
+export default function ColdplayPage() {
+	return (
+		<main>
+			<section className="container">
+				{ content.map((album, index) => (
+					<MusicDisplay key={ album.title } index={ index } components={ album } />
+				)) }
+				<br />
+			</section>
+		</main>
+	);
+}
+
+export let metadata: Metadata = {
+    title: "Coldplay Albums"
+};
