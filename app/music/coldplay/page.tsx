262327777c689adc907859024726009417894ff5
fisherbyu <fisherandrew777@gmail.com>
2024-03-04 09:45:29 -0700

    add title

--- app/music/coldplay/page.tsx
+++ app/music/coldplay/page.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
 import MusicDisplay from "@/components/ui/music-display";
+import PageTitle from "@/components/ui/page-title";
 
 // Import Content
 import content from "@/app/music/coldplay/content.json";
 
+const title: { title: string; subtitle?: string; center?: boolean } = {
+    title: "Coldplay Albums",
+    subtitle: "My review of each Coldplay Album",
+    center: true
+}
+
 export default function ColdplayPage() {
 	return (
 		<main>
+			<PageTitle components={ title } />
 			<section className="container">
 				{ content.map((album, index) => (
 					<MusicDisplay key={ album.id } index={ index } components={ album } />
