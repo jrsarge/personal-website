e9b36a8da5459666348c4685161430d011814591
fisherbyu <fisherandrew777@gmail.com>
2024-03-04 09:42:14 -0700

    Add title

--- app/music/artists/page.tsx
+++ app/music/artists/page.tsx
@@ -1,13 +1,20 @@
 import type { Metadata } from "next";
 import MusicDisplay from "@/components/ui/music-display";
+import PageTitle from "@/components/ui/page-title";
 import content from "@/app/music/artists/content.json";
 
+const title: { title: string; subtitle?: string; center?: boolean } = {
+    title: "My Favorite Artists",
+    subtitle: "Check out some of my Favorite Artists",
+    center: true
+}
 
 export default function ArtistPage() {
 	return (
 		<main>
+			<PageTitle components={ title } />
 			{ content.map((artist, index) => (
-				<MusicDisplay key={ artist.name } index={ index } components={ artist } />
+				<MusicDisplay key={ artist.id } index={ index } components={ artist } />
 			)) }
 		</main>
 	);
