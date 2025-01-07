b61937c62b5f18614ab2742a7f7c7a7e2c3f81c6
Jacob Sargent <95953463+jrsarge@users.noreply.github.com>
2025-01-06 19:01:41 -0700

    Hero Picture and description change

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 "use client"
 // Import Images
 import Image, { StaticImageData } from "next/image";
-import ImageAF_3_2 from "@/public/home/andrew-fisher-3-2.jpg";
-import ImageAF_5_4 from "@/public/home/andrew-fisher-5-4.jpg";
+import dcpeaks from "@/public/home/dcpeaks.jpg";
 import Folsom from "@/public/home/folsom.jpg";
 import LaLaLand from "@/public/home/movies/lalaland.jpg";
 import WalterMitty from "@/public/home/movies/waltermitty.jpg"
@@ -84,26 +83,41 @@ const movies: { title?: string; caption?: string; mdcol: number; lgcol: number; 
 // Hero Header
 function HeroBlock() {
     return (
-        <section className="container">
-            <div className="flex flex-col lg:flex-row justify-center items-stretch">
-                <div id="img block" className="mt-6 md:mt-8 lg:mt-0 lg:w-8/12 ">
-                    <div className="relative w-full h-full">
-                            <Image src={ ImageAF_3_2 } alt="Picture of Andrew Fisher" className="w-full h-full relative hidden lg:block rounded" />
-                            <Image src={ ImageAF_5_4 } alt="Picture of Andrew Fisher" className="md:mx-auto w-full h-full lg:hidden rounded" />
-                    </div>
-                </div>
-                <div id="text block" className="flex justify-center items-center order-first md:mx-auto md:w-11/12 md:mt-12 lg:w-5/12 lg:px-6 lg:order-none">
-                    <div>
-                        <h1 className=" dark:text-white text-4xl md:text-5xl xl:text-6xl font-semibold text-gray-900 w-10/12">
-                                I'm Andrew!
-                        </h1>
-                        <p className="dark:text-gray-300 md:w-10/12 lg:w-11/12 xl:w-10/12 mt-4 lg:mt-5 text-base leading-normal text-gray-600">
-                                I'm a Master of Information Systems Managment Student at BYU interested in software development.    I love serving and getting to know people and using technology to create powerful solutions.    I love music and music history and am an avid cook and baker.    Thanks for visiting my site!     
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <section className="relative w-full h-screen">
+  <div className="flex flex-col lg:flex-row justify-center items-stretch">
+    {/* Image block */}
+    <div
+      id="img-block"
+      className="relative mt-0 md:mt-0 lg:mt-0 lg:w-16/24"
+    >
+      <div className="relative w-full h-full">
+        <Image
+          src={dcpeaks}
+          alt="Picture of Jacob Sargent"
+          className="w-full h-full relative hidden lg:block rounded"
+        />
+      </div>
+    </div>
+
+    {/* Text block */}
+    <div
+        id="text-block"
+        className="absolute inset-0 flex flex-col justify-start items-end text-right p-6 md:p-16 bg-black bg-opacity-0 text-white inline-block"
+    >
+        <h1 className="text-4xl md:text-5xl xl:text-9xl font-semibold">
+            Who is he?
+        </h1>
+        <p className="mt-2 lg:mt-7 text-base leading-normal">
+            Master of Information Systems Student at BYU. <br></br>
+            Winner of multiple national competitons for Information Systems.<br></br>
+            Multi-Ultra Trail Marathon finisher.<br></br>
+            Loyal friend, husband and a true people person.<br></br>
+            <br></br>
+            Welcome to the site.
+        </p>
+    </div>
+  </div>
+</section>
     )
 }
 
